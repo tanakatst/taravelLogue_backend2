147f0071bc499767cd485d8427fca2fd34f03ad2
tanakatst <110578391+tanakatst@users.noreply.github.com>
2023-04-08 04:35:37 +0900

    Revert "sanctumの導入と、認証系機能実装"

--- travellogue-api/database/seeders/DatabaseSeeder.php
+++ travellogue-api/database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +18,5 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this -> call(UserSeeder::class);
     }
 }
--- travellogue-api/routes/api.php
+++ travellogue-api/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,12 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/register',[LoginController::class, 'register']);
-Route::post('/logout', [LoginController::class, 'logout']);
-Route::group(['middleware' => 'auth:sanctum'], function(){
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-});
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
