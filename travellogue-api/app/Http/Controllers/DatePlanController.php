f6559b7fdb603083faa2a877440157a5ebc37aac
Sota Tanaka <sohtacareer12@gmail.com>
2023-04-08 04:31:51 +0900

    sanctumto

---
+++ travellogue-api/app/Http/Controllers/DatePlanController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreDatePlanRequest;
+use App\Http\Requests\UpdateDatePlanRequest;
+use App\Models\DatePlan;
+
+class DatePlanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreDatePlanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DatePlan $datePlan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DatePlan $datePlan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateDatePlanRequest $request, DatePlan $datePlan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DatePlan $datePlan)
+    {
+        //
+    }
+}
---
+++ travellogue-api/app/Http/Controllers/PostImageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePostImageRequest;
+use App\Http\Requests\UpdatePostImageRequest;
+use App\Models\PostImage;
+
+class PostImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostImageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PostImage $postImage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PostImage $postImage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostImageRequest $request, PostImage $postImage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PostImage $postImage)
+    {
+        //
+    }
+}
